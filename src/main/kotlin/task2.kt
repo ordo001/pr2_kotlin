1b9dc1498c5155d4280063cbb74e750bf981965e
ordo001 <ordooo@mail.ru>
2024-09-14 12:17:32 +0300

    v1.12

---
+++ src/main/kotlin/task2.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    val array: Array<Array<Int>> = Array(size = 4)
+    {
+        Array(size = 3)
+        {
+            (0.. 9).random()
+        }
+    }
+
+
+
+}
